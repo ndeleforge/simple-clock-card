79b6ece251c915e50631ae5a3db31ac29d4b64b1
ndeleforge <contact@ndeleforge.fr>
2024-11-17 18:17:56 +0100

    indentation gone

--- simple-clock-card.js
+++ simple-clock-card.js
@@ -14,78 +14,81 @@ class SimpleClock extends HTMLElement {
         }
     }
 
-displayClock() {
-    this.getTime();
-    this.getDate();
-    setInterval(() => this.getTime(), 1000);
-    setInterval(() => this.getDate(), 60000);
-}
-
-getTime() {
-    const now = new Date();
-    let timeOptions = { hour: '2-digit', minute: '2-digit' };
-    if (this.showSeconds) {
-        timeOptions.second = '2-digit';
+    displayClock() {
+        this.getTime();
+        this.getDate();
+        setInterval(() => this.getTime(), 1000);
+        setInterval(() => this.getDate(), 60000);
     }
 
-    let time = now.toLocaleTimeString([], timeOptions);
-    this.timeDiv.innerHTML = time;
-    this.timeDiv.style.fontSize = this.hourFontSize;
-    aaqzsdsqfsd
-    if (this.hourBoldText === true) {
-        this.timeDiv.style.fontWeight = "bold";
-    }
-}
+    getTime() {
+        const now = new Date();
+        let timeOptions = { hour: '2-digit', minute: '2-digit' };
 
-getDate() {
-    const now = new Date();
-    let dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
-    if (this.showYear === true) {
-        dateOptions.year = 'numeric';
-    }
+        if (this.showSeconds) {
+            timeOptions.second = '2-digit';
+        }
 
-    let date = now.toLocaleDateString(this.localeDate, dateOptions).replace(',', '');
+        let time = now.toLocaleTimeString([], timeOptions);
 
-    if (this.dateCapitalize === true) {
-        date = date.replace(/\b\w/g, char => char.toUpperCase());
+        this.timeDiv.innerHTML = time;
+        this.timeDiv.style.fontSize = this.hourFontSize;
+
+        if (this.hourBoldText === true) {
+            this.timeDiv.style.fontWeight = "bold";
+        }
     }
 
-    this.dateDiv.innerHTML = date;
-    this.dateDiv.style.fontSize = this.dateFontSize;
-}
+    getDate() {
+        const now = new Date();
+        let dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
+        
+        if (this.showYear === true) {
+            dateOptions.year = 'numeric';
+        }
 
-createCard() {
-    const card = document.createElement('ha-card');
-    this.content = document.createElement('div');
-    this.content.style.padding = '15px';
-    this.content.style.textAlign = 'center';
+        let date = now.toLocaleDateString(this.localeDate, dateOptions).replace(',', '');
 
-    this.timeDiv = document.createElement('div');
-    this.timeDiv.style.lineHeight = '1em';
-    this.content.appendChild(this.timeDiv);
+        if (this.dateCapitalize === true) {
+            date = date.replace(/\b\w/g, char => char.toUpperCase());
+        }
 
-    this.dateDiv = document.createElement('div');
-    this.dateDiv.style.lineHeight = '1em';
-    this.content.appendChild(this.dateDiv);
+        this.dateDiv.innerHTML = date;
+        this.dateDiv.style.fontSize = this.dateFontSize;
+    }
 
-    card.appendChild(this.content);
-    this.appendChild(card);
-}
+    createCard() {
+        const card = document.createElement('ha-card');
+        this.content = document.createElement('div');
+        this.content.style.padding = '15px';
+        this.content.style.textAlign = 'center';
 
-setConfig(config) {
-    this.config = config;
-    this.hourFontSize = config.hour_font_size || '5em';
-    this.hourBoldText = Boolean(config.hour_bold_text);
-    this.showSeconds = Boolean(config.show_seconds);
-    this.dateFontSize = config.date_font_size || '2em';
-    this.dateCapitalize = Boolean(config.date_capitalize);
-    this.showYear = Boolean(config.show_year);
-    this.localeDate = config.locale_date || 'en-US';
-}
+        this.timeDiv = document.createElement('div');
+        this.timeDiv.style.lineHeight = '1em';
+        this.content.appendChild(this.timeDiv);
 
-getCardSize() {
-    return 1;
-}
+        this.dateDiv = document.createElement('div');
+        this.dateDiv.style.lineHeight = '1em';
+        this.content.appendChild(this.dateDiv);
+
+        card.appendChild(this.content);
+        this.appendChild(card);
+    }
+
+    setConfig(config) {
+        this.config = config;
+        this.hourFontSize = config.hour_font_size || '5em';
+        this.hourBoldText = Boolean(config.hour_bold_text);
+        this.showSeconds = Boolean(config.show_seconds);
+        this.dateFontSize = config.date_font_size || '2em';
+        this.dateCapitalize = Boolean(config.date_capitalize);
+        this.showYear = Boolean(config.show_year);
+        this.localeDate = config.locale_date || 'en-US';
+    }
+
+    getCardSize() {
+        return 1;
+    }
 }
 
 customElements.define('simple-clock', SimpleClock);
