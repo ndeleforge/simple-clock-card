1072564e8a6360b1db3a7333e6b914d8d1e5fefd
ndeleforge <contact@ndeleforge.fr>
2025-04-07 19:44:46 +0200

    new options to custom the clock

--- README.md
+++ README.md
@@ -50,6 +50,7 @@ type: custom:simple-clock
 | -------------- | ------------ | --------------- | ------- | -----------------
 | type | Yes | Name of the card | string | none
 | no_background | No | Hide the background card | boolean | false
+| layout | No | If set to "row", hour and date will be displayed in one row | string | twoRows
 | **Hour settings**
 | hour_font_size | No | Define hour font size | string | 5em
 | hour_bold_text | No | Put time in bold | boolean | false
@@ -61,3 +62,4 @@ type: custom:simple-clock
 | month_format | No | Put the month in *short* or *long* format | string | long
 | show_year | No | Show year | boolean | false
 | locale_date | No | Define locale for date | string | en-US
+| break_date | No | Break the date into two parts | boolean | false
--- simple-clock-card.js
+++ simple-clock-card.js
@@ -1,6 +1,6 @@
 class SimpleClock extends HTMLElement {
     static NAME = "Simple Clock Card";
-    static VERSION = "0.2.4";
+    static VERSION = "0.2.5";
 
     set hass(hass) {
         if (!this.content) {
@@ -15,69 +15,127 @@ class SimpleClock extends HTMLElement {
     }
 
     getClock() {
+        // Get time and date a first time
         this.getTime();
         this.getDate();
-        setInterval(() => this.getTime(), 1000);
-        setInterval(() => this.getDate(), 60000);
+        
+        // Get time every second
+        setInterval(() => {
+            this.getTime();
+            const now = new Date();
+            if (now.getSeconds() === 0) {
+                this.getDate();
+            }
+        }, 1000);
     }
 
     getTime() {
+        // Define time options
         const now = new Date();
         let timeOptions = { hour: '2-digit', minute: '2-digit' };
         
+        // Add second if the option is set
         if (this.isTrue(this.showSeconds)) timeOptions.second = '2-digit';
         
+        // Set locale time
         let time = now.toLocaleTimeString([], timeOptions);
         
+        // Add it to the time container
         this.timeDiv.innerHTML = time;
         this.timeDiv.style.fontSize = this.hourFontSize;
         
+        // Add bold if the option is set
         if (this.isTrue(this.hourBoldText)) this.timeDiv.style.fontWeight = "bold";
     }
 
     getDate() {
+        // Define date options
         const now = new Date();
         let dateOptions = { day: 'numeric' };
         
+        // Weekday and month format
         dateOptions.weekday = (this.weekdayFormat === 'short') ? 'short' : 'long';
         dateOptions.month = (this.monthFormat === 'short') ? 'short' : 'long';
         
+        // Add year if the option is set
         if (this.isTrue(this.showYear)) dateOptions.year = 'numeric';
         
+        // Set local date
         let date = now.toLocaleDateString(this.localeDate, dateOptions).replace(',', '');
         
+        // Add capitalization if the option is set
         if (this.isTrue(this.dateCapitalize)) {
             date = date.split(' ')
                 .map(word => word.charAt(0).toUpperCase() + word.slice(1))
                 .join(' ');
         }
         
+        // Add "er" for french date only 
         if (this.localeDate.startsWith('fr')) date = date.replace(/\b1\b/, '1er');
         
+        // Cut the date in two parts if the option is set
+        if (this.isTrue(this.dateBreak)) {
+            const parts = date.split(' ');
+            
+            // Depends on if the year is displayed or not
+            if (parts.length === 3) {
+                const firstLine = parts[0];
+                const secondLine = parts.slice(1).join(' ');
+                date = `${firstLine}<br>${secondLine}`;
+            } else if (parts.length === 4) {
+                const firstLine = parts.slice(0, 2).join(' ');
+                const secondLine = parts.slice(2).join(' ');
+                date = `${firstLine}<br>${secondLine}`;
+            }
+        }
+        
+        // Add it to the date container
         this.dateDiv.innerHTML = date;
         this.dateDiv.style.fontSize = this.dateFontSize;
     }
 
     setCard() {
+        // Define card
         const card = document.createElement('ha-card');
         
+        // Remove background and shadow if the option is set
         if (this.isTrue(this.noBackground)) {
             card.style.background = 'none'; 
             card.style.boxShadow = 'none'; 
         }
         
+        // Create the main container
         this.content = document.createElement('div');
         this.content.style.padding = '15px';
         this.content.style.textAlign = 'center';
         
+        // Switch the container to flex style if option is set
+        this.content.style.display = (this.layout === 'row') ? 'flex' : 'block';
+        if (this.layout === "row") {
+            this.content.style.flexDirection = 'row';
+            this.content.style.justifyContent = 'space-evenly';
+            this.content.style.alignItems = 'center';
+            this.content.style.gap = '10px';
+        }
+        
+        // Create the time container
         this.timeDiv = document.createElement('div');
         this.timeDiv.style.lineHeight = '1em';
-        this.content.appendChild(this.timeDiv);
+        this.content.appendChild(this.timeDiv); 
         
+        // Create the date container
         this.dateDiv = document.createElement('div');
         this.dateDiv.style.lineHeight = '1em';
+        
+        // Force the date aligned at left if some options are set together
+        if (this.layout === "row" && this.isTrue(this.dateBreak)) {
+            this.dateDiv.style.textAlign = 'left';
+            this.dateDiv.style.whiteSpace = 'pre-line';
+        }
+        
         this.content.appendChild(this.dateDiv);
         
+        // Add all elements to the card
         card.appendChild(this.content);
         this.appendChild(card);
     }
@@ -85,6 +143,7 @@ class SimpleClock extends HTMLElement {
     setConfig(config) {
         this.config = config;
         this.noBackground = config.no_background || false;
+        this.layout = config.layout || 'rows';
         this.hourFontSize = config.hour_font_size || '5em';
         this.hourBoldText = config.hour_bold_text || false;
         this.showSeconds = config.show_seconds || false;
@@ -94,6 +153,7 @@ class SimpleClock extends HTMLElement {
         this.monthFormat = config.month_format || 'long';
         this.showYear = config.show_year || false;
         this.localeDate = config.locale_date || 'en-US';
+        this.dateBreak = config.date_break || false;
     }
 
     isTrue(setting) {
